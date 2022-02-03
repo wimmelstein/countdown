1d7ff413c2767f3e5d7079948bd6871222b1c4cf
Wim Wiltenburg <wim.wiltenburg@mendesgans.nl>
2022-02-03 13:42:58 +0100

    Create Countdown Timer

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Title title="Countdown Timer" />
+  <CountDown />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import CountDown from './components/CountDown.vue'
+import Title from './components/Title.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Title,
+    CountDown
   }
 }
 </script>
